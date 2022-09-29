c7386c7213c33767f6bbf8f9926bf641d45402ff
Alexey Yanvarev <yanvarev_2015@mail.ru>
2022-09-29 12:05:13 +0400

    feat: add responsive to app

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -10,26 +10,14 @@ import { validateChartData } from '../../utils/validateChartData'
 
 import './index.scss';
 
-const data = {
-    date: "1953-06-10",
-    hour: "16",
-    av: [18, 36, 34, 37, 26, 36, -28, 17, -8, 32],
-    bv: [-37, 31, 20, 15, -3, -27, -4, 27, 9, -4],
-    cv: [20, 0, -32, 19, 24, 34, 13, -32, 37, -27],
-    dv: [-2, 12, 33, -9, 25, 23, 10, 22, -31, -8],
-    ev: [-15, 7, 29, -19, 30, 36, -40, -14, -1, -15],
-    fv: [-22, 7, 25, 0, 23, 26, 13, -3, -12, -9],
-    gv: [-2, -4, 29, 2, -25, -16, 2, 28, 21, -15]
-}
-
 const Chart = () => {
     const [chartData, setChartData] = useState([]);
     const dispatch = useDispatch();
     const [isFetching, setIsFetching] = useState(true);
+
     // мемоизировать цвета, чтобы одинаковый цвет графиков был
     const colors = useMemo(() => generateColor(chartLineNames.length), [chartLineNames.length]);
-
-    // const coordinates = useSelector(getCoordinatesSelector);
+    const coordinates = useSelector(getCoordinatesSelector);
 
     const scrollHandler = ({ target }) => {
         // clientHeight + scrollTop = clientHeight
@@ -41,7 +29,6 @@ const Chart = () => {
     useEffect(() => {
         if (isFetching) {
                 // dispatch(getData());
-                setChartData([...chartData, ...validateChartData(data)]);
                 setIsFetching(false);
         }
         
@@ -49,10 +36,10 @@ const Chart = () => {
 
     return (
         <div className='chart' onScroll={scrollHandler}>
-            <ResponsiveContainer>
+            <ResponsiveContainer width={300} height={400}>
                 <LineChart
                     layout='vertical'
-                    data={chartData}
+                    data={coordinates}
                     margin={{
                         top: 20,
                         right: 40,
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -16,7 +16,7 @@ const Table = () => {
 
     useEffect(() => {
         if (isFetching) {
-                // dispatch(getPersons());
+                dispatch(getPersons());
                 setIsFetching(false);
         }
         
